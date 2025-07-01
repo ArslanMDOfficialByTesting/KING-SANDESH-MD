63e4175aa1b2e91d29603646803dba40395d9daa
ArslanMDOfficialByTesting <muhammadarslanmdofficial@gmail.com>
2025-07-01 17:40:07 +0500

    Update index.js

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ async function downloadSessionData() {
         return false;
     }
 
-    const sessdata = config.SESSION_ID.split("KSMD~")[1];
+    const sessdata = config.SESSION_ID.split("ARSL~")[1];
 
     if (!sessdata || !sessdata.includes("*")) {
         console.error('❌ Invalid SESSION_ID format! It must contain both file ID and decryption key.');
